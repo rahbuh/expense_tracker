177c591eea7f3a813a9e96ba2411b76a908e0f7f
Robert Kramer <robert.n.kramer@gmail.com>
2020-03-06 22:52:43 -0800

    move db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const db = process.env.MONGO_URI
+const db = process.env.MONGODB_URI
 
 const connectDB = async () => {
   try {
